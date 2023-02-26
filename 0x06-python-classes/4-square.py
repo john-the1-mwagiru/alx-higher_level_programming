ee6d888d6571c794ec0367302a1992bfce6760e5
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-02-26 10:45:47 +0000

    getter and setters

---
+++ 0x06-python-classes/4-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+"""Setters and getters"""
+
+class Square:
+
+	def __init__(self,size):
+		self._size=size
+
+		if not isinstance(size,int):
+			raise TypeError("size must be an interger")
+		elif size <0:
+			raise ValueError("size must be >=0")
+	
+	def size (self,value):
+		self._size=value
