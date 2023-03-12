b2292f25101bcc9f8675a095d74fef1683adf939
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-03-12 17:15:53 +0000

    corrections

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -32,14 +32,14 @@ class Rectangle:
 		raise ValueError("width must be >= 0")
 	self.__width=value
 
-   @property
-   def height(self):
+    @property
+    def height(self):
 	"""Fetch width """
-   self.__height=height
+	 self.__height=height
 
 
-   @height.setter
-   def height(self,value):
+    @height.setter
+    def height(self,value):
 	"""Set value for height property"""
    	if not isinstance (value,int):
 		raise TypeError("height must be an interger")
@@ -47,10 +47,10 @@ class Rectangle:
 		raise ValueError("height must be >= 0")
 	self.__height=value
 
-   def area(self):
+    def area(self):
 	"""Find area of my rectangle"""
 	return(self.__height*self.__width)
 
-   def perimeter(self):
+    def perimeter(self):
 	"""Find rectangle peremeter"""
 	return(self.__height*2 + self.__width*2)
