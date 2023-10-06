608e1daaa726b04708ed616d7f0ef1e927397f24
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-06 11:11:26 +0300

    task-0-0x07:created a task folder that will be used to run all my tests and also created a python module to run ordinary python scripts

---
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+"""This module defines a function that adds 2 integers a,b
+a,b are either integers or floats 
+"""
+
+
+def add_integer(a, b=98):
+    """Compute and return the sum of two numbers
+
+
+    Args:
+        a(int): A number representing the first addend in the addition.
+        b(int): A number representing the second addend in the addition.
+    Returns:
+        int: A number representing the arithmetic sum of `a` and  `b`.
+    """
+    if not isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
