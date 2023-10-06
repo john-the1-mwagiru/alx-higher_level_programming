ffc4b2bb1bdd1eb86b158a226e80a10097ce6c57
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-06 11:52:36 +0300

    task-0-0x07:modified my function by creating exceptions to handle errors

--- 0x07-python-test_driven_development/0-add_integer.py
+++ 0x07-python-test_driven_development/0-add_integer.py
@@ -14,10 +14,8 @@ def add_integer(a, b=98):
     Returns:
         int: A number representing the arithmetic sum of `a` and  `b`.
     """
-    if (
-        isinstance(a, int)
-        and isinstance(a, float)
-        and isinstance(b, int)
-        and isinstance(b, float)
-    ):
-        return int(a) + int(b)
+    if isinstance(a, int) and not isinstance(a, float):
+        raise TypeError("a must be an integer")
+    if not isinstance(b, int) and not isinstance(b, float):
+        raise TypeError("b must be an integer")
+    return int(a) + int(b)
