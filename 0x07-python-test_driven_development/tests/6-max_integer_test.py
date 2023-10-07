91fb5dbd75a468eb9fa5c1e6fd9d8dd9421f132c
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-07 14:44:17 +0300

    task-5-0x07:added docstrings to my module and test functions to give a brief description on the what the tests are for

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,27 +1,34 @@
 import unittest
 
+"""Unittests for max_integer()"""
+
 max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxIntger(unittest.TestCase):
+    """Defines unittests for max_integer()"""
+
     def test_max(self):
+        """Tests a normal list"""
         self.assertEqual(max_integer([3, 1, 7, 4]), 7)
         self.assertEqual(max_integer([-1, -3, -10, 0]), 0)
 
     def test_floats(self):
+        """Tests a list of floats"""
         self.assertEqual(max_integer([1.1, 1.0, 7.7, 9.8]), 9.8)
 
     def test_floats_and_ints(self):
+        """Tests a list of floats and integers"""
         self.assertEqual(max_integer([9.9, 30, 29.9]), 30)
 
-    def test_values(self):
-        self.assertRaises(TypeError, max_integer, 10)
-
     def test_strings(self):
+        """Tests strings"""
         self.assertEqual(max_integer("John"), "o")
 
     def test_empty_strings(self):
+        """Tests empty strings"""
         self.assertEqual(max_integer(""), None)
 
     def test_empty_list(self):
+        """Tests an empty list"""
         self.assertEqual(max_integer([]), None)
