ed87d0792429a1d4c8bf8a2f566d31b953dd25b7
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-07 14:38:57 +0300

    task-5-0x07:modified my test function by adding in more test cases

--- 0x07-python-test_driven_development/tests/6-max_integer_test.py
+++ 0x07-python-test_driven_development/tests/6-max_integer_test.py
@@ -1,12 +1,27 @@
 import unittest
 
-print_square = __import__("6-max_integer").max_integer
+max_integer = __import__("6-max_integer").max_integer
 
 
 class TestMaxIntger(unittest.TestCase):
     def test_max(self):
-        self.assertEqual(print_square([3, 1, 7, 4]), 7)
-        self.assertEqual(print_square([-1, -3, -10, 0]), 0)
+        self.assertEqual(max_integer([3, 1, 7, 4]), 7)
+        self.assertEqual(max_integer([-1, -3, -10, 0]), 0)
+
+    def test_floats(self):
+        self.assertEqual(max_integer([1.1, 1.0, 7.7, 9.8]), 9.8)
+
+    def test_floats_and_ints(self):
+        self.assertEqual(max_integer([9.9, 30, 29.9]), 30)
 
     def test_values(self):
-        self.assertRaises(TypeError, print_square, 10)
+        self.assertRaises(TypeError, max_integer, 10)
+
+    def test_strings(self):
+        self.assertEqual(max_integer("John"), "o")
+
+    def test_empty_strings(self):
+        self.assertEqual(max_integer(""), None)
+
+    def test_empty_list(self):
+        self.assertEqual(max_integer([]), None)
