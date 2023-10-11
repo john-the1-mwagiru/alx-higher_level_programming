e1a55875de776ed2fc3b93ebcf96f203a4ecdbe6
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-12 00:21:31 +0300

    added doctstrings to describe the args and what the functions returns

--- 0x0A-python-inheritance/2-is_same_class.py
+++ 0x0A-python-inheritance/2-is_same_class.py
@@ -3,7 +3,14 @@
 
 
 def is_same_class(obj, a_class):
-    """This method returns True if the object is exactly an instance of the specified class otherwise False"""
+    """This method returns True if the object is exactly an instance of the specified class otherwise False
+    Args:
+        obj(any): The object to check
+        a_class(type): The class to match the type of obj to.
+    Returns:
+        if obj is exactly an instance of a_class  - True
+        otherwise - False
+    """
     if isinstance(obj, a_class):
         return True
     return False
