4f6c1ae508f3b830f36ef17b2fcf9535d89afc27
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-12 02:35:33 +0300

    task-6-0x0A:created a class BaseGeometry (based on 5-base_geometry.py).

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines an empty class"""
+
+
+class BaseGeometry:
+    """Defines an empty class"""
+
+    def area(self):
+        """Public instance method that raises an Exception"""
+        Exception("area() is not implemented")
