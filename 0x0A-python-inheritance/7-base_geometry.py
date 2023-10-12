715e6b3f7b29378417ce9547e190532c48cd52bb
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-12 14:05:34 +0300

    task-7-0x0A:modified my exception messages

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,25 +10,8 @@ class BaseGeometry:
         Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """This method validates the value
-        >>> geometry = BaseGeometry()
-        >>> geometry.integer_validator("my_int",12)
-        >>> geometry.integer_validator("width",89)
-        >>> geometry.integer_validator("name","john")
-        Traceback (most recent call last):
-            ...
-        TypeError: name must be an integer
-        >>> geometry.integer_validator("age",0)
-        Traceback (most recent call last):
-            ...
-        ValueError: age must be greater than 0
-        >>> geometry.integer_validator("distance",-4)
-        Traceback (most recent call last):
-            ...
-        ValueError: distance must be greater than 0
-
-        """
+        """This method validates the value"""
         if type(value) != int:
-            raise TypeError("{} must be an integer".format(name))
+            raise TypeError("<name> must be an integer")
         if value <= 0:
-            raise ValueError("{} must be greater than 0".format(name))
+            raise ValueError("<name> must be greater than 0")
