1cc1fccc392c5d6f7924e1da0c70703a8fe12b04
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-12 13:42:42 +0300

    task-7-0x0A:modified the base geometry function by using type in place of isinstance and added a few more test cases to my test file

--- 0x0A-python-inheritance/7-base_geometry.py
+++ 0x0A-python-inheritance/7-base_geometry.py
@@ -10,7 +10,24 @@ class BaseGeometry:
         Exception("area() is not implemented")
 
     def integer_validator(self, name, value):
-        """This method validates the value"""
+        """This method validates the value
+        >>> geometry = BaseGeometry()
+        >>> geometry.integer_validator("my_int",12)
+        >>> geometry.integer_validator("width",89)
+        >>> geometry.integer_validator("name","john")
+        Traceback (most recent call last):
+            ...
+        TypeError: name must be an integer
+        >>> geometry.integer_validator("age",0)
+        Traceback (most recent call last):
+            ...
+        ValueError: age must be greater than 0
+        >>> geometry.integer_validator("distance",-4)
+        Traceback (most recent call last):
+            ...
+        ValueError: distance must be greater than 0
+
+        """
         if type(value) != int:
             raise TypeError("{} must be an integer".format(name))
         if value <= 0:
