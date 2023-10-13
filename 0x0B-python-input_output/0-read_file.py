6bc1c3646578d84b2b6c309c506fa75a2d7b3582
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-13 20:02:06 +0300

    task-0-0x0B:modified my function by adding more doctsrings and used print instead of return

--- 0x0B-python-input_output/0-read_file.py
+++ 0x0B-python-input_output/0-read_file.py
@@ -3,6 +3,13 @@
 
 
 def read_file(filename=""):
+    """function that reads a text file (UTF8) and prints it to stdout
+    Args:
+        filename: file that will acts as our input
+    Returns:
+        file details to our stdout
+
+    """
     with open(filename, "r", encoding="utf-8") as filename:
         new_file = filename.read()
         print(new_file)
