468b32ecabfa5158ea33e2f973a62ad20be15068
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-13 21:04:30 +0300

    task-2-0x0B:modified my function to return characters added instead of printing

--- 0x0B-python-input_output/2-append_write.py
+++ 0x0B-python-input_output/2-append_write.py
@@ -5,4 +5,4 @@
 def append_write(filename="", text=""):
     """function that appends a string at the end of a text file (UTF8) and returns the number of characters written"""
     with open(filename, "a", encoding="utf-8") as f:
-        print(f.write(text))
+        return f.write(text)
