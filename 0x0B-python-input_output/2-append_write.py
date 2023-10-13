603addb59a7fed780766ee2e01e145bf1492dfd6
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-13 20:53:05 +0300

    task-2-0x0B:created  function that appends a string to a text file (UTF8) and returns the number of characters written

---
+++ 0x0B-python-input_output/2-append_write.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+"""Defines a text file-appending function"""
+
+
+def append_file(filename="", text=""):
+    """function that appends a string at the end of a text file (UTF8) and returns the number of characters written"""
+    with open(filename, "a", encoding="utf-8") as f:
+        print(f.write(text))
