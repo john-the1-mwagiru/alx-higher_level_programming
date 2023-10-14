f642d4b2ef898027a90707bfe6782d69aa4cce8b
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-14 13:09:19 +0300

    task-6-0x0B:created a function that creates an Object from a “JSON file”

---
+++ 0x0B-python-input_output/6-load_from_json_file.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+
+"""This module defines a function that creates an object from a json file"""
+import json
+
+
+def load_from_json_file(filename):
+    """This function creates an object from a json file"""
+    with open(filename, "r", encoding="utf-8") as filename:
+        file = json.loads(filename)
+        filename.read(file)
