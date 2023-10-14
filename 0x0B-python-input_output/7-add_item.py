df89f3feb11af3815a5f7c1ec060857ca4038130
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-14 19:05:42 +0300

    task-7-0x0B:created a cript that adds all arguments to a Python list, and then save them to a file

--- 0x0B-python-input_output/7-add_item.py
+++ 0x0B-python-input_output/7-add_item.py
@@ -1,10 +1,15 @@
 #!/usr/bin/python3
-"""This module adds all arguments to a Python list, and then save them to a file"""
 
-save_to_json_file = __import__("5-save_to_json_file").save_to_json_file
-load_from_json_file = __import__("6-load_from_json_file").load_from_json_file
+"""Add all arguments to a Python list and save them to a file."""
+import sys
 
+if __name__ == "__main__":
+    save_to_json_file = __import__("7-save_to_json_file").save_to_json_file
+    load_from_json_file = __import__("8-load_from_json_file").load_from_json_file
 
-def add_all_arguments(my_obj, filename="add_item.json"):
-    save_to_json_file(my_obj, filename="add_item.json")
-    load_from_json_file(filename="add_item.json")
+    try:
+        items = load_from_json_file("add_item.json")
+    except FileNotFoundError:
+        items = []
+    items.extend(sys.argv[1:])
+    save_to_json_file(items, "add_item.json")
