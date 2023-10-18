9af9f2a5547d7c57129aa19eb5afdbcebb2548b1
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-18 06:12:08 +0300

    task-2-0x0C:created a class Rectangle that inherits from Base

---
+++ 0x0C-python-almost_a_circle/models/rectangle.py
@@ -0,0 +1,56 @@
+#!/usr/bin/python3
+"""Defines a class Rectangle that inherits from the class Base"""
+Base = __import__("base").Base
+
+
+class Rectangle(Base):
+    """This defines a class Rectangle that inherits from Base class"""
+
+    def __init__(self, width, height, x=0, y=0, id=None):
+        """Instantiates Rectangle instances"""
+        self.width = width
+        self.height = height
+        self.x = x
+        self.y = y
+
+        super().__init__(id)
+
+    @property
+    def width(self):
+        """Retrieves the width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, width):
+        """Sets the the value of the width attribute"""
+        self.__width = width
+
+    @property
+    def height(self):
+        """Retrieves the height attribute value"""
+        return self.__height
+
+    @height.setter
+    def height(self, height):
+        """Sets the value of the height attribute"""
+        self.__height = height
+
+    @property
+    def x(self):
+        """Retrieves the x attribute value"""
+        return self.__x
+
+    @x.setter
+    def x(self, x):
+        """Sets the x attribute value"""
+        self.__x = x
+
+    @property
+    def y(self):
+        """Retrieves the y attribute value"""
+        return self.__y
+
+    @y.setter
+    def y(self, y):
+        """Sets the y attribute value"""
+        self.__y = y
