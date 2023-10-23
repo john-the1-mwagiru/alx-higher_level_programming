1ba8088e34f34210593a955bb0e0b717d224d6ca
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-23 10:57:25 +0300

    task-17-0x0C:updated the class Base by adding the static method def from_json_string(json_string): that returns the list of the JSON string representation json_string

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -22,3 +22,26 @@ class Base:
         if list_dictionaries is None:
             return "[]"
         return json.dumps(list_dictionaries)
+
+    @classmethod
+    def save_to_file(cls, list_objs):
+        """Write the JSON serialization of a list of objects to a file.
+
+        Args:
+            list_objs (list): A list of inherited Base instances.
+        """
+        filename = cls.__name__ + ".json"
+        with open(filename, "w") as jsonfile:
+            if list_objs is None:
+                jsonfile.write("[]")
+            else:
+                list_dicts = [o.to_dictionary() for o in list_objs]
+                jsonfile.write(cls.to_json_string(list_dicts))
+
+    @staticmethod
+    def from_json_string(json_string):
+        """Static method that returns the list of the json string representation json_string"""
+        if json_string is None:
+            return "[]"
+        list_lists = json.loads(json_string)
+        return list_lists
