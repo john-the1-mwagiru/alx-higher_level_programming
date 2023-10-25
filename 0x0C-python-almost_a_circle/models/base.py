9e50ba00cd45ea93f2650594001b074369d456f9
john-the1-mwagiru <johnmwagiru199@gmail.com>
2023-10-25 09:27:52 +0300

    task-19-0x0C:updated the class Base by adding the class method def load_from_file(cls): that returns a list of instances

--- 0x0C-python-almost_a_circle/models/base.py
+++ 0x0C-python-almost_a_circle/models/base.py
@@ -45,3 +45,33 @@ class Base:
             return "[]"
         list_lists = json.loads(json_string)
         return list_lists
+
+    @classmethod
+    def create(cls, **dictionary):
+        """class method that returns an instance with all attributes already set"""
+
+        if dictionary and dictionary != {}:
+            if cls.__name__ == "Rectangle":
+                new = cls(1, 1)
+            else:
+                new = cls(1)
+            new.update(**dictionary)
+            return new
+
+    @classmethod
+    def load_from_file(cls):
+        """Return a list of classes instantiated from a file of JSON strings.
+
+        Reads from `<cls.__name__>.json`.
+
+        Returns:
+            If the file does not exist - an empty list.
+            Otherwise - a list of instantiated classes.
+        """
+        filename = str(cls.__name__) + ".json"
+        try:
+            with open(filename, "r") as jsonfile:
+                list_dicts = Base.from_json_string(jsonfile.read())
+                return [cls.create(**d) for d in list_dicts]
+        except IOError:
+            return []
